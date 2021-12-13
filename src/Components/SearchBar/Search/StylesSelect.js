34deb94d6364c31c7af57bcff1c529db058eac74
Alexey <Letonik3@gmail.com>
2021-12-14 00:02:19 +0300

    start2

--- src/Components/SearchBar/Search/StylesSelect.js
+++ src/Components/SearchBar/Search/StylesSelect.js
@@ -70,38 +70,3 @@ export const stylesState = {
         }
     })
 }
-
-/*    select {
-      height: 45px;
-      width: 100%;
-      box-sizing: border-box;
-      background: rgba(255, 255, 255, 0);
-      border-radius: 4px;
-      border: 2px solid $colorWhite;
-      color: $colorWhite;
-      option {
-        color: black;
-      }
-    }*/
-
-/*    @media only screen and (max-width: 767px) {
-     flex-direction: column;
-      div{
-        width: 100%;
-      }
-    }
-    @media only screen and (min-width: 1260px) {
-      flex-direction: row;
-      .countryWrapper {
-        margin-left: 12px;
-        select {
-          border-radius: 4px 0 0 4px;
-          border-right: none;
-        }
-      }
-      .stateWrapper {
-        select {
-          border-radius: 0 4px 4px 0;
-        }
-      }
-    }*/
